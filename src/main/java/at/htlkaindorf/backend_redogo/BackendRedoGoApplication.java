ca6afb34c5fb020164dcfe5b8c385a1271ee9017
Nici0211 <strnib22@htl-kaindorf.at>
2026-09-16 14:15:07 +0200

    Initial commit

---
+++ src/main/java/at/htlkaindorf/backend_redogo/BackendRedoGoApplication.java
@@ -0,0 +1,13 @@
+package at.htlkaindorf.backend_redogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRedoGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendRedoGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/htlkaindorf/backend_redogo/BackendRedoGoApplicationTests.java
@@ -0,0 +1,13 @@
+package at.htlkaindorf.backend_redogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRedoGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
